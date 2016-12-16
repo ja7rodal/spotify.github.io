4083c5d7b55c3132aaf108fbb39e5f5836bfea47
Jasson Rdz <ja7rodal@gmail.com>
2016-12-15 20:24:36 -0500

    functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
+var showTracks = function(a) {
+  var source = $('#tracks-template').html();
+  var template = Handlebars.compile(source);
+  $.getJSON("https://api.spotify.com/v1/search",{q:a, type: "track"})
+              .done(function(data){
+                  var html = template(data.tracks);
+                  $('.view').html(html);
+              }).fail(function() {
+                console.log( "error" );
+              })
+              .always(function() {
+                console.log( "complete" );
+              });
+ }
 
+$('#search-music').on('keyup', function(e) {
+  e.preventDefault;
+  if(e.which == 13){
+    var a = $('#search-music').val();
+    $('#search-music').val('');
+    showTracks(a);
+  }
+});
